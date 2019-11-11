df1aa8dcaef5b7811722bfdc364d83b933be067c
SccSwimmer02 <55505952+SccSwimmer02@users.noreply.github.com>
2019-11-11 13:24:29 -0500

    Add files via upload

---
+++ AppRunner.java
@@ -0,0 +1,14 @@
+package mainPackage;
+
+import java.applet.Applet;
+import java.awt.*;
+import javax.swing.*;
+
+public class AppRunner extends Applet {
+	public void paint(Graphics g) {
+		super.paint(g);
+	}
+	public String toString() {
+		
+	}
+}
